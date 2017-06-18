59f8355a101b455642d4c88430bad66934e3942d
gavin <zhang159916@gmail.com>
2017-06-18 21:22:26 +0800

    add test

--- public/v/js/cali.bodytest.js
+++ public/v/js/cali.bodytest.js
@@ -19,7 +19,11 @@ require.config({
         //https://github.com/kazupon/vue-i18n
         "vue-i18n":"https://cdn.bootcss.com/vue-i18n/7.0.3/vue-i18n",
         //https://github.com/moment/moment
-        "moment":"https://cdn.bootcss.com/moment.js/2.15.1/moment.min"
+        "moment":"https://cdn.bootcss.com/moment.js/2.15.1/moment.min",
+        //self
+        // "header":"/public/v/js/cali.header",
+        // "footer":"/public/v/js/cali.footer",
+        // 'i18n':"/public/v/js/cali.i18n"
     },
     shim: {
         'jquery': {
@@ -38,86 +42,40 @@ require.config({
             exports:'fetch'
         },
         'vue-i18n': {
-            deps: ['vue'],
-            //exports: 'Backbone'
+            deps: ['vue']
         },
         'moment':{
             exports:'moment'
-        }
+        },
+        //self
+        // 'header':{
+        //     exports:'commonData',
+        //     deps:["jquery","store","vue"]
+        // },
+        // 'footer':{
+        //     deps:["jquery","store","vue"]
+        // },
+        // 'i18n':{
+        //     //exports:'getI18n',
+        //     deps:["jquery","store","vue","vue-i18n"]
+        // }
     }
 });
 require(['jquery', 'bootstrap', 'underscore','showdown','pagination','store','vue','vue-i18n','fetch','moment'], function ($, bootstrap, _, showdown,pagination,store,Vue,VueI18n,fetch,moment){
     // some code here
-    var math = _.min([0,1,2,3,4,-1]);
-    $("#math").text(math);
-
-    var converter = new showdown.Converter(),
-        text      = '#hello, markdown!',
-        html      = converter.makeHtml(text);
-    $("#markdown").text(html);
-
-
-    $('#testpage').pagination({
-        dataSource:function (done) {
-            var tmp = [];
-            for(var i=0; i<100;i++){
-                tmp.push(i);
-            }
-            return done(tmp);
-        },
-        pageRange:1,
-        totalNumber:100,
-        pageSize: 8,
-        showGoInput: true,
-        showGoButton: true,
-        callback: function(data, pagination) {
-            $("#pagecallback").text(data);
-        }
-    });
-
-    store.set("sss","sssok");
-    $("#pagecallback").text(store.get('sss'));
-
-    var data = new FormData();
-    data.set("bookid",Request.bookid);
-    fetch('/api/book/book',{method:'post',body:data}).then(function(response) {
-        if (response.redirected){
-            window.location.href = response.url;
-        }
-        return response.json();
-    }).then(function(json) {
-        alert(json.statusCode);
-    }).
-    catch(function(ex) {
-        console.log('parsing failed', ex)
-    });
-
-    Vue.use(VueI18n);
-    const messages = {
-        en: {
-            lang: {
-                search:"Search"
-            }
-        },
-        cn: {
-            lang: {
-                search:"搜索"
-            }
-        }
-    };
-    const i18n = new VueI18n({
-        locale: 'en', // set locale
-        messages, // set locale messages
-    });
-
     var app = new Vue({
-        i18n,
-        el: "#test",
-        data: {
-            ok:"okkkkk  vue"
+        el: "#root",
+        data: {},
+        methods: {},
+        computed: {},
+        created: function() {
+            console.log("created");
+        },
+        beforeMount: function () {
+            console.log("beforeMount");
+        },
+        mounted: function () {
+            console.log("mounted");
         }
     });
-
-
-    $("#moment").text(moment(new Date()).format("YYYY-MM-DD"))
 });
